0e6b1b4d73d7e1b3ddee0ef29b67af6876d993bf
JustARandomDudette <85719116+JustARandomDudette@users.noreply.github.com>
2021-10-17 17:49:48 -0500

    Add files via upload

---
+++ luchat.js
@@ -0,0 +1,3 @@
+function login_now() {
+window.location = "luchat_room.html";
+}
---
+++ luchat_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAww8R0aUg3yLB-KAcD6Scv7cNDMHfYBXc",
+    authDomain: "projectluchat.firebaseapp.com",
+    projectId: "projectluchat",
+    storageBucket: "projectluchat.appspot.com",
+    messagingSenderId: "863182859100",
+    appId: "1:863182859100:web:7119411e9ae46ace97ef99"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
